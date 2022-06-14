6975cba9810d18156952a63cd6eba5211567299e
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-06-14 09:24:29 +0100

    feat: add method and test to remove product of cart

--- src/cart/Cart.spec.js
+++ src/cart/Cart.spec.js
@@ -4,9 +4,13 @@ from './Cart'
 
 describe('Cart', () => {
   let cart;
-  let product = {
+  let productOne = {
     title: 'Adidas',
     price: 120
+  }
+  let productTwo = {
+    title: 'Nike',
+    price: 100
   }
 
   // before each test
@@ -21,7 +25,7 @@ describe('Cart', () => {
 
   it('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product,
+      product: productOne,
       quantity: 2
     }
     cart.add(item)
@@ -32,14 +36,29 @@ describe('Cart', () => {
   it('should ensure no more than on product quantity exists at a time', () => {
 
     cart.add({
-      product,
+      product: productOne,
       quantity: 2
     })
     cart.add({
-      product,
+      product: productOne,
       quantity: 1
     })
 
     expect(cart.getTotal()).toEqual(240)
+  });
+
+  it('should update total when a product gets included then removed ', () => {
+    cart.add({
+      product: productOne,
+      quantity: 2
+    })
+    cart.add({
+      product: productTwo,
+      quantity: 1
+    })
+
+    cart.remove(productOne)
+
+    expect(cart.getTotal()).toEqual(100)
   });
 });
