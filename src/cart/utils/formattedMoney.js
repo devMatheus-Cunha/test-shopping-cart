bf25ee321a446991c66a8aa08434b8a5c4c94b58
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-06-17 10:15:13 +0100

    refactor: export utils

---
+++ src/cart/utils/formattedMoney.js
@@ -0,0 +1,10 @@
+import Dinero from "dinero.js"
+
+const Money = Dinero
+
+Money.defaultCurrency = "BRL"
+Money.defaultPrecision = 2
+
+export {
+  Money
+}
---
+++ src/cart/utils/index.js
@@ -0,0 +1,11 @@
+import {
+  calculateDiscount,
+} from './discount.js'
+import {
+  Money
+} from './formattedMoney.js'
+
+export {
+  calculateDiscount,
+  Money,
+}
